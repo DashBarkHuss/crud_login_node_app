585eb51b8b75c4fdd86c6ce5ea7cdb7d71561c03
Dashiell BarkHuss <dashiellbarkhuss@Dashiells-MacBook-Pro.local>
2019-08-11 11:02:43 -0500

    serving html files

--- index.js
+++ index.js
@@ -3,4 +3,14 @@ const http = require('http');
 
 http.createServer((request, response)=>{
     console.log("req: ",request.url);
+    let file;
+    request.url == "/"? file = 'index.html': file = request.url.slice(1, request.url.length);
+    fs.readFile(file, (err, data) => {
+        if (err){
+            console.log(err);
+        } else {
+            response.writeHead(200, {'Content-Type':'text/html'});
+            response.end(data, 'utf-8');
+        }
+    })
 }).listen(3000, '127.0.0.1')
